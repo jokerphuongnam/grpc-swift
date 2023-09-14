aef8e11c6e638c2ed475ae413b981706683f3185
Phạm P. Nam <nampp@inspirelab.io>
2023-09-14 14:35:12 +0700

    change nio to tag 2.57.0

--- Package.swift
+++ Package.swift
@@ -32,7 +32,7 @@ let includeNIOSSL = ProcessInfo.processInfo.environment["GRPC_NO_NIO_SSL"] == ni
 let packageDependencies: [Package.Dependency] = [
   .package(
     url: "https://github.com/apple/swift-nio.git",
-    from: "2.42.0"
+    .exactItem("2.57.0")
   ),
   .package(
     url: "https://github.com/apple/swift-nio-http2.git",
